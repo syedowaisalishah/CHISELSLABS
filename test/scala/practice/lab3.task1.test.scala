d40b6938ba25ef5eca766834d7902b320e8ea833
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2022-09-12 11:44:17 +0500

    Update lab3.task1.test.scala

--- test/scala/practice/lab3.task1.test.scala
+++ test/scala/practice/lab3.task1.test.scala
@@ -5,7 +5,7 @@ import chiseltest._
 class TASK1TEST extends FreeSpec with ChiselScalatestTester{
     "TASK1 TESTING" in {
         test(new BranchControl()){c=>
-            c.io.func3.poke(1.U)
+            c.io.fnct.poke(1.U)
             c.io.branch.poke(1.B)
             c.io.arg_x.poke(4.U)
             c.io.arg_y.poke(3.U)
