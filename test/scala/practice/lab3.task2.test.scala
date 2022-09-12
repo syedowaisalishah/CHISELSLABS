1e8e03ca0eb26fdf8ebe72352b382adcc7f391a8
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2022-09-12 11:47:06 +0500

    Update lab3.task2.test.scala

--- test/scala/practice/lab3.task2.test.scala
+++ test/scala/practice/lab3.task2.test.scala
@@ -5,9 +5,9 @@ import chiseltest._
 class TASK2TEST extends FreeSpec with ChiselScalatestTester{
     "TASK2 TESTING" in {
         test(new Imm_dValGen()){c=>
-        c.io.instr.poke()
+        c.io.instr.poke("h00600093".U)
         c.clock.step(1)
-        c.io.immd_se.expect()
+        c.io.immd_se.expect(6.U)
         }
 
     }
