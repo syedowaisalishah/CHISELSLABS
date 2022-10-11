d6b9867e876db217c52280c2c2454938709e5957
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2022-10-11 09:50:01 +0500

    Add files via upload

---
+++ main/scala/practice/lab4_task1.scala
@@ -0,0 +1,35 @@
+// package practice
+// import chisel3 . _
+// import chisel3 . util . _
+// class LM_IO_Interface_BranchControl extends Bundle {
+//     val fnct3 = Input ( UInt (3. W ) )
+//     val branch = Input ( Bool () )
+//     val arg_x = Input ( UInt (32. W ) )
+//     val arg_y = Input ( UInt (32. W ) )
+//     val br_taken = Output ( Bool () )
+// }
+// class BranchControl extends Module {
+//     val io = IO (new LM_IO_Interface_BranchControl )
+// // Start Coding here
+//     val out=Wire(Bool())
+//     out:=0.B
+//     switch(io.fnct3){
+//         is("b000".U){
+//             out:=io.arg_x === io.arg_y}
+//         is("b001".U){
+//             out:=io.arg_x =/= io.arg_y}
+//         is("b101".U){
+//             out:=io.arg_x > io.arg_y}
+//         is("b100".U){
+//             out:=io.arg_x < io.arg_y}
+//         is("b110".U){
+//             out:=io.arg_x <= io.arg_y}
+//         is("b111".U){
+//             out:=io.arg_x >= io.arg_y}
+//     }
+//     io.br_taken:=io.branch && out
+// }
+// // End your code here
+// // Well , you can actually write classes too . So , technically you have no
+
+
